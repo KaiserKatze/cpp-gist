d4955c2e3bcfb46ff840d5e9ad2b0f442c074868
KaiserKatze <donizyo@gmail.com>
2025-03-18 12:30:10 +0800

    Fix 3726a51

--- convert_encoding.py
+++ convert_encoding.py
@@ -7,7 +7,7 @@ import logging
 import pathlib
 
 
-def convert_encoding(root_dir, src_encoding='utf-8', dst_encoding='utf-8-sig'):
+def convert_encoding(root_dir, src_encoding='gb2312', dst_encoding='utf-8-sig'):
     for root, _, files in os.walk(root_dir):
         path_root = pathlib.Path(root)
         for file in files:
@@ -19,6 +19,7 @@ def convert_encoding(root_dir, src_encoding='utf-8', dst_encoding='utf-8-sig'):
                         content = f.read()
                 except UnicodeDecodeError:
                     logging.error(f'按 {src_encoding} 编码，从文件 [{str(file_path)}] 读取内容时，发生错误!')
+                    continue
                 # 转换
                 try:
                     with codecs.open(file_path, 'w', encoding=dst_encoding) as f:
@@ -27,12 +28,24 @@ def convert_encoding(root_dir, src_encoding='utf-8', dst_encoding='utf-8-sig'):
                     logging.error(f'按 {dst_encoding} 编码，从文件 [{str(file_path)}] 写入内容时，发生错误!')
 
 
-def show_file_dialog():
+def show_directory_dialog():
     import tkinter as tk
     from tkinter import filedialog
-    folder_path = filedialog.askdirectory(initialdir=os.curdir)
+    folder_path = filedialog.askdirectory(title='选择需要转换编码的文件', initialdir=os.curdir)
     if folder_path and os.path.isdir(folder_path):
         convert_encoding(folder_path)
+
+
+def show_file_dialog():
+    from tkinter import filedialog
+    file_path = filedialog.askopenfilenames(title='选择需要转换编码的文件', initialdir=os.curdir, multiple=True)
+    if not file_path:
+        return
+    file_path = [pathlib.Path(os.curdir) / file for file in file_path]
+    if not all(file.is_file() for file in file_path):
+        return
+    for file in file_path:
+        convert_encoding(file, src_encoding='utf-8', dst_encoding='utf-8-sig')
 
 
 if __name__ == '__main__':
