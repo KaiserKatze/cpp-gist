0773ff9cd69c48e468a88e0153469da7c4932ab1
KaiserKatze <donizyo@gmail.com>
2025-10-13 11:51:29 +0800

    Update

---
+++ Test/MissionaryAndCannibal.hpp
@@ -0,0 +1,269 @@
+﻿#pragma once
+
+// “传教士和食人族”问题
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cassert>
+#include <bitset>
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
+
+struct MissionaryAndCannibal {
+    struct State {
+        const int countMax;     // 传教士、食人族的个数
+        int nMissionaries;      // 当前状态下，在左岸的传教士个数
+        int nCannibals;         // 当前状态下，在左岸的食人族个数
+        bool direction;          // 确定船只航向方向
+
+        State(int countMax, int nMissionaries, int nCannibals, bool direction) :
+            countMax{ countMax },
+            nMissionaries{ nMissionaries },
+            nCannibals{ nCannibals },
+            direction{ direction } {
+            if (nMissionaries > countMax || nMissionaries < 0) {
+                throw std::invalid_argument{ "Invalid `nMissionaries`!" };
+            }
+            if (nCannibals > countMax || nCannibals < 0) {
+                throw std::invalid_argument{ "Invalid `nCannibals`!" };
+            }
+        }
+
+        bool IsValid() const {  // 检查状态是否合法，合法则返回 true，否则返回 false
+            const auto ml{ GetMissionaryLeft() };
+            const auto mr{ GetMissionaryRight() };
+            const auto cl{ GetCannibalLeft() };
+            const auto cr{ GetCannibalRight() };
+            return (ml == 0 || ml >= cl) && (mr == 0 || mr >= cr);
+        }
+
+        int GetMissionaryLeft() const {
+            return nMissionaries;
+        }
+
+        int GetCannibalLeft() const {
+            return nCannibals;
+        }
+
+        int GetMissionaryRight() const {
+            return countMax - nMissionaries;
+        }
+
+        int GetCannibalRight() const {
+            return countMax - nCannibals;
+        }
+
+        int GetMissionary() const {
+            return direction ? GetMissionaryLeft() : GetMissionaryRight();
+        }
+
+        int GetCannibal() const {
+            return direction ? GetCannibalLeft() : GetCannibalRight();
+        }
+
+        int GetDirectionAsInt() const {  // 返回船只朝向
+            return direction ? 1 : -1;
+        }
+
+        size_t GetDirectionAsIndex() const {
+            return direction ? 1 : 0;
+        }
+
+        State Move(int deltaMissionaries, int deltaCannibals) const {
+            const int newMissionaries{ nMissionaries - GetDirectionAsInt() * deltaMissionaries };
+            const int newCannibals{ nCannibals - GetDirectionAsInt() * deltaCannibals };
+            return State{
+                countMax,
+                newMissionaries,
+                newCannibals,
+                !direction,  // 调转船头
+            };
+        }
+
+        explicit operator std::string() const {
+            std::stringstream ss;
+            ss << "状态( "
+                << std::dec
+                // << "左岸传教士人数最大值=" << countMax << ", "
+                << "左岸传教士=" << GetMissionaryLeft() << ", "
+                << "左岸食人族=" << GetCannibalLeft() << ", "
+                << "右岸传教士=" << GetMissionaryRight() << ", "
+                << "右岸食人族=" << GetCannibalRight() << ", "
+                << "船头朝向=" << (direction ? "朝右" : "朝左") << ", "
+                // << std::boolalpha
+                // << "合法性=" << IsValid()
+                << ")";
+            return ss.str();
+        }
+
+        bool operator==(const State& other) const {
+            return countMax == other.countMax
+                && nMissionaries == other.nMissionaries
+                && nCannibals == other.nCannibals
+                && direction == other.direction;
+        }
+
+        bool operator!=(const State& other) const {
+            return countMax != other.countMax
+                || nMissionaries != other.nMissionaries
+                || nCannibals != other.nCannibals
+                || direction != other.direction;
+        }
+    };
+
+    static void Solve() {
+        const int countMax{ 3 };        // 传教士、食人族的个数
+        const int shipCapacity{ 2 };    // 船只最大可以容纳的乘客个数
+        // 初始状态
+        const State initialState{ countMax, countMax, countMax, true /* 一开始船头朝着右岸 */ };
+        // 目标状态
+        const State finalState{ countMax, 0, 0, false /* 目标状态船头朝着左岸 */ };
+
+        std::cout << "初始状态：" << static_cast<std::string>(initialState) << '\n';
+
+        // 记录访问标记
+        const size_t dimension{ static_cast<size_t>(countMax) + 1 };
+        std::vector<std::vector<std::vector<bool>>> visited(dimension, std::vector<std::vector<bool>>(dimension, std::vector<bool>(2, false)));
+        // 记录合法状态
+        std::vector<State> validStates;
+        // 记录路径
+        std::vector<int> route;
+        // 缓存需要探索的状态（在 `validStates` 中的序号）
+        std::queue<int, std::deque<int>> moveTo;
+
+        const auto IsVisited = [&](const State& state) {
+            return visited[state.nMissionaries][state.nCannibals][state.GetDirectionAsIndex()];
+        };
+        const auto Visit = [&](const State& state) {
+            visited[state.nMissionaries][state.nCannibals][state.GetDirectionAsIndex()] = true;
+        };
+        const auto AddValidState = [&](int prec, const State& next) {
+            Visit(next);
+            route.push_back(prec);              // `next` 的前趋状态在 `validStates` 中的序号
+            moveTo.push(validStates.size());    // 记录 `next` 在 `validStates` 中的序号
+            validStates.push_back(next);        // 存储合法状态
+        };
+        const auto GetState = [&](int indexInvalidState) {
+            return validStates.at(static_cast<size_t>(indexInvalidState));
+        };
+
+        // 状态空间大小
+        const size_t countState{ dimension * dimension * 2 };
+        // 状态转移变换个数
+        const size_t countOperator{ countState * (countState - 1) / 2 };
+
+        std::cout << "状态空间大小：" << countState << '\n'
+            << "算子数量：" << countOperator << '\n';
+
+        AddValidState(-1, initialState);  // 初始状态是合法状态，初始状态没有前趋状态
+
+        size_t nLoop { 0 }, limitLoop{ countOperator + 5 }; // 防止死循环
+        bool reachFinalState{ false };
+        while (
+            !moveTo.empty()  // 还有需要检查的中间状态
+            && !reachFinalState  // 尚未出现连接初始状态和目标状态的合法路径
+        ) {
+            // 取出当前状态（先取出一个序号，再到 `validStates` 中查表）
+            const int indexCurrentState{ moveTo.front() };
+            moveTo.pop();
+            const State& currentState{ GetState(indexCurrentState) };
+            std::cout
+                << "===========================\n"
+                << "当前状态：" << static_cast<std::string>(currentState) << '\n'
+                << "\t依次尝试不同的乘船安排 ...\n";
+            // 依次尝试不同的乘船安排
+            const int supMissionary{ min(shipCapacity, currentState.GetMissionary()) };  // 可以乘船的传教士人数
+            std::cout << "\t可以乘船的传教士人数：" << supMissionary << '\n';
+            for (int x{ 0 }; x <= supMissionary; ++x) {
+                const int supCannibal{ min(shipCapacity - x, currentState.GetCannibal()) };  // 可以乘船的食人族人数
+                std::cout << "\t当乘船的传教士人数为 " << x << " 时，可以乘船的食人族人数：" << supCannibal << '\n';
+                for (int y{ 0 }; y <= supCannibal; ++y) {
+                    if (++nLoop > limitLoop) {
+                        std::cerr << "\t超出预计的循环次数！\n";
+                        return;
+                    }
+                    if (x + y == 0) {  // 传教士和食人族都不动
+                        continue;  // 抛弃这种没用的乘船安排
+                    }
+                    std::cout
+                        << "\t----------------\n"
+                        << "\t预备乘船的传教士人数：" << x << '\n'
+                        << "\t预备乘船的食人族人数：" << y << '\n';
+                    const State newState{ currentState.Move(x, y) };
+                    const bool isNewStateValid{ newState.IsValid() };
+                    const bool isNewStateVisited{ IsVisited(newState) };
+                    const bool isFinalState{ newState == finalState };
+                    std::cout
+                        << "\t检查新状态：" << static_cast<std::string>(newState) << '\n'
+                        << std::boolalpha
+                        << "\t是否已访问新状态：" << isNewStateVisited << '\n'
+                        << "\t是否目标状态：" << isFinalState
+                        << "\n\n";
+                    if (!isNewStateValid) {  // 状态非法（食人族把传教士吃掉了）
+                        std::cout
+                            << "\t跳过非法状态：" << static_cast<std::string>(newState) << '\n';
+                        Visit(newState);
+                        continue;
+                    }
+                    if (isNewStateVisited) {  // 已经访问过这个中间状态了
+                        std::cout
+                            << "\t跳过已访问过的中间状态：" << static_cast<std::string>(newState) << '\n';
+                        continue;
+                    }
+                    reachFinalState = isFinalState;
+                    AddValidState(indexCurrentState, newState);  // 更新合法状态表，更新需要探索的状态
+                }
+            }
+
+            std::cout << "需要检查的中间状态还有 " << moveTo.size() << " 个 ...\n";
+            if (moveTo.size() > countState) {
+                throw std::runtime_error{ "中间状态个数超过状态空间大小!" };
+            }
+        }
+
+        finalStage:
+        {
+            size_t countVisitedStates{ 0 };
+            for (const auto visitedRow : visited) {
+                for (const auto visitedCell : visitedRow) {
+                    for (const auto visitedElem : visitedCell) {
+                        if (visitedElem) {
+                            ++countVisitedStates;
+                        }
+                    }
+                }
+            }
+            std::cout << "循环结束时，已访问过 " << countVisitedStates << " 个状态（包括初始状态在内）.\n";
+        }
+
+        if (validStates.back() != finalState) {
+            std::cout << "没有找到通往目标状态的合法路径!\n";
+        } else {
+            std::cout << "成功找到通往目标状态的合法路径：\n";
+            std::stack<State, std::vector<State>> backtrace;
+            int prec{ *std::crbegin(route) };
+            backtrace.push(validStates.back());
+            while (prec != -1) {
+                backtrace.push(validStates.at(prec));
+                prec = route.at(prec);
+            }
+            size_t index{ 0 };
+            while (!backtrace.empty()) {
+                State& node{ backtrace.top() };
+                std::cout << "#" << ++index
+                    << '\t'
+                    << static_cast<std::string>(node)
+                    << '\n';
+                backtrace.pop();
+            }
+        }
+    }
+};
+
+// 抽样的原则：代表性、随机性、可靠性
+// 抽样方法：简单随机抽样、系统抽样（等距抽样）、分层抽样
+// 样本分析方法：描述统计、推断统计
+// 假设检验的底层逻辑：全称命题只能被证否而不能被证明.
+// 假设检验所依据的基本原理是小概率原理.
--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <sstream>
 #include <stdexcept>
 #include <cstring>
@@ -12,18 +12,32 @@
 #include <vector>
 #include <compare> // std::strong_ordering (since C++20)
 #include <memory>
+#include <chrono>
 
 #include <Windows.h>
 #include <fcntl.h>
 #include <io.h>
 
 #define DEBUG
-#include "RLYC.hpp"
+#include "MissionaryAndCannibal.hpp"
 
 int main(int argc, char** argv) {
-    SetConsoleOutputCP(65001);
+    // SetConsoleOutputCP(65001);
 
-    RLYC::Solve();
+    const auto now{ std::chrono::system_clock::now() };
+    const std::time_t now_time_t{ std::chrono::system_clock::to_time_t(now) };
+    const std::tm* now_tm{ std::localtime(&now_time_t) };
+
+    std::cout << "当前时间："
+        << std::put_time(now_tm, "[%Y-%m-%d %H:%M:%S] ")
+        << '\n';
+
+    try {
+        MissionaryAndCannibal::Solve();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
 
     return 0;
 }
